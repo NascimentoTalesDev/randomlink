7a8590f38a055fe4271b019c42a8565c48003b76
NascimentoTalesDev <nascimentotalesdev@gmail.com>
2024-11-01 01:31:02 -0300

    init

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,9 +3,9 @@
 export async function getUrls(){
 
     const urls = [
-        'https://wa.me/5541987269386?text=Opa%20bom%20dia%201',
-        'https://wa.me/5541987269386?text=Opa%20bom%20dia%202',
-        'https://wa.me/5541987269386?text=Opa%20bom%20dia%203'
+        'https://wa.me/5541988259336?text=Opa%20bom%20dia%201',
+        'https://wa.me/5541983849386?text=Mais%20um%20numero',
+        'https://wa.me/5541935163386?text=Outro%20numero'
     ]
 
     return urls
